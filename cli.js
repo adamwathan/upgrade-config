26150247c93b9ece1bb2e22724503b364705d6ee
Adam Wathan <adam.wathan@gmail.com>
2019-03-18 16:25:29 -0400

    Format output file

--- cli.js
+++ cli.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const prettier = require('prettier')
 const [,, ...args] = process.argv
 
 const upgradeConfig = require('./index')
 
-fs.writeFileSync('./tailwind.config.js', upgradeConfig(args[0]))
+const format = code => prettier.format(code, { semi: false, singleQuote: true, parser: 'babel' })
+
+fs.writeFileSync('./tailwind.config.js', format(upgradeConfig(args[0])))
