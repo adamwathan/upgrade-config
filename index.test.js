dcc1e005dbf22bfa77298271e012047ddb36a73c
Jason McCreary <jason@pureconcepts.net>
2019-03-29 16:15:47 -0400

    Boy scout test

--- index.test.js
+++ index.test.js
@@ -1,18 +1,12 @@
 const upgrade = require('./index')
-const stringifyObject = require('stringify-object')
 const prettier = require('prettier')
 const fs = require('fs')
 
-const stringify = s => stringifyObject(s, {
-  indent: '  ',
-  inlineCharacterLimit: 100,
-})
-
 const format = code => prettier.format(code, { parser: 'json' })
 
-test('it upgrades the config format', () => {
-  const newConfig = fs.readFileSync('./fixtures/output.js', 'utf8')
-  const upgradedConfig = upgrade('./fixtures/input.js')
+test('it upgrades a simple config', () => {
+  const expected = fs.readFileSync('./fixtures/output.js', 'utf8')
+  const actual = upgrade('./fixtures/input.js')
 
-  expect(format(upgradedConfig)).toEqual(format(newConfig))
+  expect(format(actual)).toEqual(format(expected))
 })
