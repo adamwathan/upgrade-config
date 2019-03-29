c7af964908d1cb821f7c2c676eff1224edc304af
Jason McCreary <jason@pureconcepts.net>
2019-03-29 16:05:59 -0400

    Hijack require instead of regex approach

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Module = require('module')
 const fs = require('fs')
 const path = require('path')
 const _ = require('lodash')
@@ -8,60 +9,114 @@ const stringify = s => stringifyObject(s, {
   inlineCharacterLimit: 100,
 })
 
-const configTemplate = `
-module.exports = {
-  prefix: %%PREFIX%%,
-  important: %%IMPORTANT%%,
-  separator: %%SEPARATOR%%,
-  theme: %%THEME%%,
-  variants: %%VARIANTS%%,
-  plugins: %%PLUGINS%%,
-}
-`
-
 function upgradeConfig(configPath) {
-  const config = require(path.resolve(configPath))
-  const configFileContents = fs.readFileSync(path.resolve(configPath), 'utf8')
+  // load the config, but don't `require` modules
+  // instead create an intermediate
+  // representation to evaluate later so certain
+  // calls to `require` can be preserved
+  // e.g. plugins
+  const originalRequire = Module.prototype.require
 
-  return _.flow([
-    addOptionsSections(config),
-    addPluginsSection(configFileContents),
-    addThemeSection(config),
-    addVariantsSection(config),
-  ])(configTemplate)
-}
-
-function addOptionsSections(config) {
-  return configTemplate => {
-    return configTemplate
-      .replace('%%PREFIX%%', `'${config.options.prefix}'`)
-      .replace('%%IMPORTANT%%', `${config.options.important}`)
-      .replace('%%SEPARATOR%%', `'${config.options.separator}'`)
+  Module.prototype.require = function(moduleName) {
+    return options => ({ _require: moduleName, _options: options  })
   }
-}
 
-function addThemeSection(config) {
-  return ([configTemplate, containerOptions]) => {
-    return configTemplate.replace('%%THEME%%', stringify(upgradeThemeProperties(config, containerOptions)))
+  var config = originalRequire(path.resolve(configPath))
+
+  Module.prototype.require = originalRequire
+
+
+  // store "special keys" and remove them from
+  // config so it can be parsed later
+  const plugins = config.plugins || [];
+  delete config.plugins;
+
+
+  // re-`require` any intermediate representation
+  // on the config object so all the config options
+  // are in one object
+  const mapValuesDeep = (obj) => {
+    if (_.isArray(obj)) {
+      return obj.map(innerObj => mapValuesDeep(innerObj));
+    }
+
+    if (_.isObject(obj)) {
+      if (_.isEqual(_.sortBy(_.keys(obj)), _.sortBy(['_require', '_options']))) {
+        // TODO: what if _require is "defaultConfig"
+
+        return require(obj._require)(obj._options)
+      }
+
+      return _.mapValues(obj, val => mapValuesDeep(val));
+    }
+
+    return obj;
+  };
+
+  config = mapValuesDeep(config)
+
+  // TODO: remap to new tailwindcss options
+  config = {
+    ...config.options,
+    theme: extractThemeProperties(config),
+    variants: extractVariantsProperties(config),
   }
-}
 
-function addVariantsSection(config) {
-  return configTemplate => {
-    return configTemplate.replace('%%VARIANTS%%', stringify(upgradeVariantsProperties(config)))
+  // now remove any intermediate representations
+  // which reference old tailwindcss modules from
+  // "special keys"
+  tailwind_plugins = _.remove(plugins, function (plugin) {
+      if (!_.isObject(plugin)) {
+        return false
+      }
+
+      if (!_.isEqual(_.sortBy(_.keys(plugin)), _.sortBy(['_require', '_options']))) {
+        return false
+      }
+
+      return plugin._require === 'tailwindcss/plugins/container'
+  })
+
+  const container = _.merge({}, ..._.map(tailwind_plugins, '_options'));
+
+  if (_.isEmpty(container)) {
+    delete config.theme.container
+  } else {
+    config.theme.container = container
   }
-}
 
-function addPluginsSection(configFileContents) {
-  return configTemplate => {
-    const [updatedTemplate, containerOptions] = buildPluginsSection(configFileContents)
-    return [configTemplate.replace('%%PLUGINS%%', updatedTemplate), containerOptions]
+  // rebuild plugins as a string representation
+  const strings = _.map(plugins, function (plugin) {
+    if (_.isObject(plugin) && _.isEqual(_.sortBy(_.keys(plugin)), _.sortBy(['_require', '_options']))) {
+      return `require('${plugin._require}')(${plugin._options ? stringify(plugin._options) : ''})`
+    }
+
+    return stringify(plugin)
+  })
+
+  // TODO: options resulting config by removing
+  // option values which match new defaults
+
+  if (strings.length) {
+    config.plugins = '%%PLUGINS%%'
   }
+
+  output = stringify(config)
+
+  return 'module.exports = ' + output.replace("'%%PLUGINS%%'", '[' + _.join(strings) + ']');
 }
 
-function upgradeThemeProperties({ options, modules, plugins, ...theme }, containerOptions) {
+function extractThemeProperties({ options, modules, plugins, ...theme }) {
+  return updateThemePropertyNames(theme)
+}
+
+function extractVariantsProperties({ modules }) {
+  return updateVariantsPropertyNames(modules)
+}
+
+function updateThemePropertyNames(theme) {
   return {
-    ..._.isEmpty(containerOptions) ? {} : { container: containerOptions },
+    container: {},
     colors: theme.colors,
     screens: theme.screens,
     fontFamily: theme.fonts,
@@ -92,7 +147,7 @@ function upgradeThemeProperties({ options, modules, plugins, ...theme }, contain
   }
 }
 
-function upgradeVariantsProperties({ modules }) {
+function updateVariantsPropertyNames(modules) {
   const propertyMap = {
     appearance: ['appearance'],
     backgroundAttachment: ['backgroundAttachment'],
@@ -161,25 +216,6 @@ function upgradeVariantsProperties({ modules }) {
       return [newKey, modules[key]]
     })
   }).fromPairs().value()
-}
-
-function buildPluginsSection(configFileContents) {
-  let pluginsContents = configFileContents.match(/^(\s+)plugins:\s+\[(.*?)^\1]/ms)[2]
-
-  if (!/require\(\s*'tailwindcss\/plugins\/container'\s*\)/.test(pluginsContents)) {
-    return [`[${pluginsContents}]`, {}]
-  }
-
-  const objectOptionsPattern = /^(\s+)require\(\s*'tailwindcss\/plugins\/container'\s*\)\s*\(\s*{(.*?)\1\}\),?/ms
-
-  if (objectOptionsPattern.test(pluginsContents)) {
-    // console.log(pluginsContents.match(objectOptionsPattern)[2])
-    const containerOptions = eval(`({\n${pluginsContents.match(objectOptionsPattern)[2]}\n})`)
-    pluginsContents = pluginsContents.replace(objectOptionsPattern, '')
-    return [`[${pluginsContents}]`, containerOptions]
-  }
-
-  // TODO: Handle weird non-object literal arguments (require(my-stupid-container-options)?)
 }
 
 module.exports = upgradeConfig
--- index.test.js
+++ index.test.js
@@ -3,13 +3,16 @@ const stringifyObject = require('stringify-object')
 const prettier = require('prettier')
 const fs = require('fs')
 
-const format = code => prettier.format(code, { semi: false, parser: 'babel' })
+const stringify = s => stringifyObject(s, {
+  indent: '  ',
+  inlineCharacterLimit: 100,
+})
+
+const format = code => prettier.format(code, { parser: 'json' })
 
 test('it upgrades the config format', () => {
-  // const legacyConfig = fs.readFileSync('./fixtures/input.js', 'utf8')
-  // const legacyConfig = require('./fixtures/input.js')
   const newConfig = fs.readFileSync('./fixtures/output.js', 'utf8')
-  // const newConfig = require('./fixtures/output.js')
+  const upgradedConfig = upgrade('./fixtures/input.js')
 
-  expect(format(upgrade('./fixtures/input.js'))).toEqual(format(newConfig))
+  expect(format(upgradedConfig)).toEqual(format(newConfig))
 })
